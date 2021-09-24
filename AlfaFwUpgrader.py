ddd5f864ceddf2f21715492b15d12d01ee206bd3
Filippo Gurgoglione <filippogurgoglione@alfadispenser.com>
2021-09-24 12:29:30 +0200

    implemented command line application

--- AlfaFwUpgrader.py
+++ AlfaFwUpgrader.py
@@ -46,6 +46,10 @@ import usb.util
 import struct
 import logging
 
+import argparse
+import sys
+import traceback
+
 from hexutils import HexUtils
 from typing import Any, NoReturn
 
@@ -310,9 +314,9 @@ class USBManager:
 class USBFirmwareLoader:
     """ Memory management of PIC24 based boards using USB protocol."""
     
-    def __init__(self):
+    def __init__(self, deviceId):
         try:
-            self.usb = USBManager()
+            self.usb = USBManager(deviceId)
         except:
             raise RuntimeError("failed to init USB device")
             
@@ -405,17 +409,156 @@ class USBFirmwareLoader:
                                    "positions {} and {}".format(
                                    cursor, cursor + chunk_len))
         return True
+
+class Application:
+    DESCRIPTION = "An utility to program Alfa PIC based boards " \
+                  "using USB based bootloader."
+    NAME = "alfa_fw_upgrader"
+    EXAMPLE_TEXT = '''examples:
+
+To perform program and verify,
+ > alfa_fw_upgrader -f master_tinting-boot.hex program verify
+
+To perform verify only, with debug info and reset,
+ > alfa_fw_upgrader -vv -f master_tinting-boot.hex verify reset'''
+
+    actions = ('program', 'verify', 'reset')
+
+    errors_dict = {
+        "FILENAME_REQUIRED": {
+            "descr": "Filename is required with selected action(s)",
+            "retcode": 1,
+        },
+        "FILE_LOAD_FAILED": {
+            "descr": "Failed to load filename {}",
+            "retcode": 2,
+            "hints": [
+                "Check if the file exists",
+                "Check if the file has a valid Intel Hex format"
+            ]
+
+        },
+        "INIT_FAILED": {
+            "descr": "Failed to initialize ({})",
+            "retcode": 3,
+            "hints": [            
+                "Check if the device is powered up and correctly connected",
+                "Check if the device is in boot mode",
+                "Check if the driver is working correctly. On windows,"
+                "run Zadig and select WinUSB driver for this device and be "
+                "sure to have library libusb-1.0.dll in c:\Windows\System32"
+            ]
+        },
+        "ERASE_FAILED": {
+            "descr": "Failed to erase ({})",
+            "retcode": 4
+        },
+        "VERIFY_FAILED": {
+            "descr": "Failed to verify ({})",
+            "retcode": 5
+        },
+        "VERIFY_DATA_MISMATCH": {
+            "descr": "Verify failed due to data mismatch",
+            "retcode": 6
+        },
+    }
+
+    def _exit_error(self, error_key, format_arg = None):
+        pr_exc = self.args.verbosity is not None
+    
+        error_item = self.errors_dict[error_key]
+        descr = error_item["descr"] if not format_arg else \
+               error_item["descr"].format(format_arg)
+        hints = ""
+        if "hints" in error_item:
+            for h in error_item["hints"]:
+                hints += " - " + h + "\n"
+                
+        text = "The following error occured during execution: "
+        text += descr + "\n"
+        if not pr_exc:
+            text += "Increment level of verbosity to get exception\n"
+        text += f"\nHints:\n{hints}\n" if hints else ""
+        text += f"Return value is {error_item['retcode']}\n"
+        sys.stderr.write(text)
+        
+        if pr_exc: 
+            traceback.print_exc(file=sys.stdout)           
+        exit(error_item["retcode"])
+        
+    def main(self):
+        parser = argparse.ArgumentParser(
+                          prog = self.NAME,
+                          description=self.DESCRIPTION,
+                          epilog=self.EXAMPLE_TEXT,
+                          formatter_class=argparse.RawDescriptionHelpFormatter)
+                           
+        parser.add_argument('-f', '--filename', dest='HEXFILENAME', type=str,
+                            help='filename of the IntelHex file to load')
+
+        parser.add_argument('-d', '--deviceid', dest='ID', type=int, default=255,
+                            help='ID of the device (default=255)')
+
+        parser.add_argument("-v", "--verbosity", action="count",
+                            help="increase output verbosity")
+
+        parser.add_argument("actions", type=str, nargs='+',  
+                            choices=self.actions,
+                            help="actions to perform")
+
+        if len(sys.argv) < 2:
+            parser.print_help()
+            sys.exit(1)
+            
+        self.args = parser.parse_args()
+
+        level = "ERROR"
+        if self.args.verbosity is not None:
+            if self.args.verbosity >= 2:
+                level = "DEBUG"
+            elif self.args.verbosity == 1:
+                level = "INFO"
+            
+        logging.basicConfig(
+            stream=sys.stdout, level=level,
+            format="[%(asctime)s]%(levelname)s %(funcName)s() "
+                   "%(filename)s:%(lineno)d %(message)s")
+
+        # put actions in the right order and avoid repetitions
+        actions = [x for x in self.actions if x in self.args.actions]
+
+        program_data = []
+        if 'program' in actions or 'verify' in actions:
+            if self.args.HEXFILENAME is None:
+                self._exit_error("FILENAME_REQUIRED")
+            fn = self.args.HEXFILENAME
+            try:
+                program_data = HexUtils.load_hex_file_to_array(fn)
+            except:
+                self._exit_error("FILE_LOAD_FAILED", fn)
+
+        try:
+            ufl = USBFirmwareLoader(self.args.ID)
+        except Exception as e:
+            self._exit_error("INIT_FAILED", str(e))
+        
+        for a in actions:
+            if a == 'program':
+                try:
+                    ufl.erase()
+                except:
+                    self._exit_error("ERASE_FAILED")                   
+                try:
+                    ufl.program(program_data)
+                except:
+                    self._exit_error("PROGRAM_FAILED")
+            elif a == 'verify':
+                try:
+                    if ufl.verify(program_data) is False:
+                        self._exit_error("VERIFY_DATA_MISMATCH")
+                except:
+                    self._exit_error("VERIFY_FAILED")
                    
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    
-    ufl = USBFirmwareLoader()
-    ufl.erase()
-    
-    fn = "Master_Tinting-boot-nodipswitch.hex"
-    program_data = HexUtils.load_hex_file_to_array(fn)
-    ufl.program(program_data)
-    
-    assert(ufl.verify(program_data) == True)
-
+    Application().main()
 
